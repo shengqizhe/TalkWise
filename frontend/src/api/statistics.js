57b9789a8c777f32d4318958b837905cadbf7954
zhishan <1719890482@qq.com>
2026-09-13 21:16:12 +0800

    chore: remove test code and dead code

--- frontend/src/api/statistics.js
+++
@@ -1,9 +0,0 @@
-import request from "../utils/request";
-
-// 获取学生统计数据
-export function getStudentStatistics(userId) {
-  return request({
-    url: `/statistics/student/${userId}`,
-    method: "get",
-  });
-}
